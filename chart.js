46294be9bd8e99f6ec7861b97cd33e0ea089dd93
TomGarrison <Tomgarrison2011@gmail.com>
2017-03-04 15:40:02 -0700

    finals

--- chart.js
+++ chart.js
@@ -4,10 +4,9 @@
 
   $('#btn').click(function() {
     $('html').toggleClass('dark')
-    $('#header').toggleClass('dark')
-    $('#con').toggleClass('dark')
     $('body').toggleClass('dark')
     $('#header').toggleClass('light')
+    $('html').toggleClass('light')
     $(".container").slideUp().animate({ opacity: 1 }, { queue: false, duration: 'fast' }).slideDown().animate({ opacity: 1 }, { queue: false, duration: 'fast' })
   })
 
